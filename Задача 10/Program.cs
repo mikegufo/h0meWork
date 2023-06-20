d25c0b7ad91d23bee323ab0e2f8af95415476705
Vadim <nikegufo666@gmail.com>
2023-06-21 02:30:52 +0500

    Добавил Задачу 10

---
+++ Задача 10/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+System.Console.WriteLine("Введите число ");
+int numberABC = Convert.ToInt32(Console.ReadLine());
+int numberB = numberABC % 100;
+int result = numberB / 10;
+
+System.Console.WriteLine(result);
